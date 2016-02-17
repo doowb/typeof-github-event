a3eee03401ad1facf70a7e457b2ffed993e488fd
Brian Woodward <brian.woodward@gmail.com>
2016-02-17 01:41:54 -0500

    implementation

--- index.js
+++ index.js
@@ -7,6 +7,37 @@
 
 'use strict';
 
-module.exports = function() {
+var events = require('./lib/event-map');
+
+module.exports = {
+  typeof: function(payload) {
+    var validate = valid(payload);
+    var keys = Object.keys(events);
+    var len = keys.length, i = -1;
+    while (++i < len) {
+      if (validate(events[keys[i]])) {
+        return keys[i];
+      }
+    }
+  },
+  is: function(event, payload) {
+    return this.typeof(payload) === event;
+  }
 };
 
+function valid(payload) {
+  return function(event) {
+    var props = event.props;
+    var len = props.length, i = -1;
+    while (++i < len) {
+      var prop = props[i];
+      if (!payload.hasOwnProperty(prop)) {
+        return false;
+      }
+    }
+    if (typeof event.validate === 'function') {
+      return event.validate(payload);
+    }
+    return true;
+  };
+}
