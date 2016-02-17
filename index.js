51969ee5c27f8420975266e3296b49115994252d
Brian Woodward <brian.woodward@gmail.com>
2016-02-16 23:54:45 -0500

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * github-event <https://github.com/doowb/github-event>
+ *
+ * Copyright (c) 2016, Brian Woodward.
+ * Licensed under the MIT License.
+ */
+
+'use strict';
+
+module.exports = function() {
+};
+
